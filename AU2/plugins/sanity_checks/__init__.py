415ed2af979798c87f70667dc12588a30a4661c7
Alexei Newton <60141408+thenewtonium@users.noreply.github.com>
2025-02-23 21:16:47 +0000

    [92] Sanity check for when player not dead but [DX] or [NX] used. (#111)
    Added sanity check to catch occurences of [DX] and [NX] when the player did not die

--- AU2/plugins/sanity_checks/__init__.py
+++ AU2/plugins/sanity_checks/__init__.py
@@ -2,8 +2,10 @@ from typing import Dict
 
 from AU2.plugins.sanity_checks.IncorrectPseudonymFormatter import IncorrectPseudonymFormatter
 from AU2.plugins.sanity_checks.MissingHtmlSpecifier import MissingHtmlSpecifier
+from AU2.plugins.sanity_checks.PlayerNotDead import PlayerNotDead
 
 SANITY_CHECKS: Dict[str, "SanityCheck"] = {
     "Missing_HTML_Specifiers": MissingHtmlSpecifier(),
-    "Incorrect_Pseudonym_Formatter": IncorrectPseudonymFormatter()
+    "Incorrect_Pseudonym_Formatter": IncorrectPseudonymFormatter(),
+    "Player_Not_Dead": PlayerNotDead()
 }
