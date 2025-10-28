41584c67ea12c686b30f39a10644ca598c2f4fc7
Alexei Newton <60141408+thenewtonium@users.noreply.github.com>
2025-10-28 17:49:01 +0000

    Enforce Python version (#151)
    * Added explicit Python 3.9+ requirement
    
    * Python 3.9+ requirement in __main__.py
    
    * Catch venv setup errors

--- AU2/__main__.py
+++ AU2/__main__.py
@@ -1,4 +1,15 @@
+import sys
 import time
+
+PYTHON_VERSION_REQUIREMENT = (3, 9)
+
+# check Python version to give a helpful message
+# need to do this before importing AU2 files because type hints can cause errors in old versions of python
+if not sys.version_info >= PYTHON_VERSION_REQUIREMENT:
+    print(f"Error: AU2 requires Python {'.'.join(str(x) for x in PYTHON_VERSION_REQUIREMENT)} or above. "
+          f"You are using Python {'.'.join(str(x) for x in sys.version_info)}.")
+    time.sleep(100)
+    exit()
 
 from AU2.frontends.inquirer_cli import main
 
