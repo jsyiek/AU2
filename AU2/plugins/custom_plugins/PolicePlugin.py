f6f89e474b2408e273f715fdfa16ca9bbd04ef86
thenewtonium <60141408+thenewtonium@users.noreply.github.com>
2025-11-05 14:29:38 +0000

    Added police config to Setup Game
    Only selection of Umpire and CoP since these are what actually matter.

--- AU2/plugins/custom_plugins/PolicePlugin.py
+++ AU2/plugins/custom_plugins/PolicePlugin.py
@@ -116,7 +116,6 @@ class PolicePlugin(AbstractPlugin):
                 answer=self.answer_set_special_ranks
             )
         ]
-
     # configs are only stored in database if changed; otherwise the hardcoded default configs are used.
 
     def gsdb_get(self, plugin_state_id):
@@ -125,6 +124,16 @@ class PolicePlugin(AbstractPlugin):
 
     def gsdb_set(self, plugin_state_id, data):
         GENERIC_STATE_DATABASE.arb_state.setdefault(self.identifier, {})[self.plugin_state[plugin_state_id]['id']] = data
+
+    def on_request_setup_game(self, game_type: str) -> List[HTMLComponent]:
+        return [
+            *self.ask_set_special_ranks(),
+        ]
+
+    def on_setup_game(self, htmlResponse) -> List[HTMLComponent]:
+        return [
+            *self.answer_set_special_ranks(htmlResponse),
+        ]
 
     def gather_dead_non_police(self) -> List[str]:
         # if police plugin is enabled we assume perma-death is enabled (i.e. it is not May Week)
@@ -173,7 +182,7 @@ class PolicePlugin(AbstractPlugin):
     def answer_set_special_ranks(self, htmlResponse):
         self.gsdb_set("Umpires", htmlResponse[self.html_ids["Umpires"]])
         self.gsdb_set("Chief of Police", htmlResponse[self.html_ids["CoP"]])
-        return [Label("[POLICE] Success!")]
+        return [Label("[POLICE] Set Umpire(s) and Chief(s) of Police")]
 
     def ask_set_ranks(self):
         question = []
