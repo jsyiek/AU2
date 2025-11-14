696211bcf0ed2994061c28c465d98ba6d9526a13
thenewtonium <60141408+thenewtonium@users.noreply.github.com>
2025-11-14 22:21:13 +0000

    `HiddenDatetime` / `HiddenJSON` after merge

--- AU2/html_components/SimpleComponents/HiddenDatetime.py
+++ AU2/html_components/SimpleComponents/HiddenDatetime.py
@@ -1,18 +1,11 @@
 import datetime
 
-from AU2.html_components import HTMLComponent
+from AU2.html_components.HiddenComponent import HiddenComponent
 from AU2.plugins.util.date_utils import dt_to_timestamp
 
 
-class HiddenDatetime(HTMLComponent):
+class HiddenDatetime(HiddenComponent[datetime.datetime]):
     name: str = "HiddenDatetime"
-    noInteraction: bool = True
-
-    def __init__(self, identifier: str, default: datetime.datetime):
-        self.identifier = identifier
-        self.uniqueStr = self.get_unique_str()
-        self.default = default
-        super().__init__()
 
     def _representation(self) -> str:
         return f"""
