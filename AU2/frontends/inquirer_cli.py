90923094d7ae0003aade0aa9e52102bb0102cd49
thenewtonium <60141408+thenewtonium@users.noreply.github.com>
2026-03-27 14:51:00 +0000

    Multi-stage kill selector
    Ensures max 1 killer per victim per event, and stops choices becoming unwieldy.

--- AU2/frontends/inquirer_cli.py
+++ AU2/frontends/inquirer_cli.py
@@ -345,25 +345,20 @@ def render(html_component, dependency_context={}):
         assassins = list(assassins_mapping.keys())
         if len(assassins) <= 1:
             return {html_component.identifier: tuple(), "skip": True}
-        potential_kills = {}
-        defaults = []
-        for a1 in assassins:
-            for a2 in assassins:
-                if a1 != a2:
-                    key = f"{a1} kills {a2}"
-                    potential_kills[key] = (a1, a2)
-                    if (a1, a2) in html_component.default:
-                        defaults.append(key)
         q = [inquirer.Checkbox(
             name="q",
-            message="Select kills",
-            choices=list(potential_kills.keys()),
-            default=defaults
+            message="Select players who DIED in this event",
+            choices=assassins,
+            default=[victim for _, victim in html_component.default],
         )]
-        # TODO: Confirm what happens if option in default isn't in choices
-        a = inquirer_prompt_with_abort(q)["q"]
-        a = tuple(potential_kills[k] for k in a)
-        return {html_component.identifier: a}
+        deaths = inquirer_prompt_with_abort(q)["q"]
+        q = [inquirer.List(
+            name=victim,
+            message=f"Who killed {escape_format_braces(victim)}?",
+            choices=[a for a in assassins if a != victim],
+        ) for victim in deaths]
+        victim_killer_mapping = inquirer_prompt_with_abort(q)
+        return {html_component.identifier: [(killer, victim) for victim, killer in victim_killer_mapping.items()]}
 
     # dependent component
     elif isinstance(html_component, AssassinDependentTransferEntry):
